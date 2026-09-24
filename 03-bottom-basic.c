566bff1639d818a9aaa6fa8887943db0a210dbb9
Shubham Kumar <74664560+sk4443060@users.noreply.github.com>
2026-09-24 23:26:51 +0530

    Add basic C program with print statements

---
+++ 03-bottom-basic.c
@@ -0,0 +1,26 @@
+/* /////////////////////////////////////////////////////////
+CHALLENGE 01. Instructions
+Use the editor to complete the following steps:
+Write a main() function
+Use printf to print Hello, C!
+End the program with return 0;
+Note: Remember curly braces and semicolons.
+#include <stdio.h>
+
+int main() {
+    printf("Hello, C!.");
+    return 0;
+}*/
+
+/* ////////////////////////////////////////////////////////
+CHALLANGE 02. Inside main(), complete the following steps:
+Use one printf to print Hello World! followed by a new line
+Use another printf to print I am learning C.
+Note: Remember to end a statement with semicolon.*/
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!\n");
+    printf("I am learning C.\n");
+    return 0;
+}
