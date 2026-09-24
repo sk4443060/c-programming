8a8e37340a80dbb6359a69e733fea5fbc6c05d54
Shubham Kumar <74664560+sk4443060@users.noreply.github.com>
2026-09-24 21:20:20 +0530

    Enhance code readability with comments
    Added comments for better code understanding.

--- 02-Hello-World.c
+++ 02-Hello-World.c
@@ -1,9 +1,9 @@
 // YOUR OWN CODE
-#include <stdio.h>
-
-int main() {
-    printf("Hi Shubham! How are you?");
-    return 0;
+#include <stdio.h>                         // <- Toolbox that contains standard input and output tools
+                                           // <- Empty space for humans to read the code easily
+int main() {                               // <- Starting Gate of your program
+    printf("Hi Shubham! How are you?");    // <- Command that Prints text on the screen
+    return 0;                              // <- This tells the computer, "Everything went perfectly, we are done!"
 }
 
 
