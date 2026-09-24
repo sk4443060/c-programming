e5b341204da1806bc86191806ab51d7e5e712750
Shubham Kumar <74664560+sk4443060@users.noreply.github.com>
2026-09-24 21:14:37 +0530

    Add Hello World program in C

---
+++ 02-Hello-World.c
@@ -0,0 +1,16 @@
+// YOUR OWN CODE
+#include <stdio.h>
+
+int main() {
+    printf("Hi Shubham! How are you?");
+    return 0;
+}
+
+
+//COPY PASTED
+// #include <stdio.h>
+
+// int main() {
+//  printf("Hi Shubham, This is your 1st program.\n");
+//  return 0;
+//}
