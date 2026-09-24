e12a8f21686935aa4b9b52b9a800557b3c245637
Shubham Kumar <74664560+sk4443060@users.noreply.github.com>
2026-09-24 23:32:45 +0530

    Add comments for variable types and declaration in C
    Added comments explaining variable types and declaration in C.

---
+++ 04-variables-in-c.c
@@ -0,0 +1,10 @@
+// int - stores whole numbers (integers), such as 123 or -123
+// float - stores numbers with decimals, such as 19.99 or -19.99
+// char - stores a single character, such as 'a' or 'B'. Characters are surrounded by single quotes
+
+// Declaring (Creating) Variables
+// type variableName = value;
+// Ex: int studentAge = 25;
+// OR
+// int studentAge;
+// studentAge = 25;
